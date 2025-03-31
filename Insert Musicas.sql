27d1ffac94ad26490e4d7cffcc74ea40d14f54dd
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-03-31 16:29:42 -0300

    Add o Insert da Tabela Musicas parcialmente feito

---
+++ Insert Musicas.sql
@@ -0,0 +1,19 @@
+INSERT INTO Musicas 
+([NumeroMusica],[NomeMusica],[Artista],[Tempo],[Codigo])
+VALUES
+(), -- Uma Música do Super
+(),
+(), -- Uma Música do Supernova
+(), -- Uma Música do Super
+(),
+(),
+(),
+(), -- Uma Música do Super
+(), -- Uma Música do Super
+(), -- Uma Música do Super
+(),  -- Uma Música do Supernova
+(),
+(),  -- Uma Música do Supernova
+(),
+(); -- Uma Música do Pirata
+GO
