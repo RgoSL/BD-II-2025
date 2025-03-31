bdde0715d699d1b10441e5d9e0fb9047f42957ce
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-03-31 16:19:52 -0300

    Insert da Tabela CDs Finalizado

--- Insert CDs.sql
+++ Insert CDs.sql
@@ -1,19 +1,19 @@
 INSERT INTO CDs
 ([Codigo], [NomeCD], [DataCompra], [ValorPago], [LocalCompra], [Album])
 VALUES
-(1, 'Super', '2023-05-15', 43.90, 'Amazon', 1), 
-(2, 'Back In Black', '2023-06-10', 45.00, 'Amazon', 0),
-(3, 'Supernova', '2023-07-20', 50.00, 'Universal Music Store', 1),
-(4, 'Super', '2023-08-25', 39.90, 'Universal Music Store', 1),
-(5, 'The Dark Side of the Moon', '2023-09-30', 42.50, 'Amazon', 0),
-(6, 'Let It Be', '2023-10-05', 126.53, 'Loja Regards', 0),	
-(7, 'Born to Run', '2023-11-10', 92.08, 'Loja Regards', 0),
-(8, 'Super', '2023-12-01', 41.90, 'Amazon', 1),
-(9, 'Super', '2024-01-15', 39.95, 'Mercado Livre', 1),
-(10, 'Super', '2024-02-05', 29.90, 'Universal Music Store', 1),
-(11, 'Supernova', '2024-03-10', 62.80, 'Mercado Livre', 1),
-(12, 'Top Hits 90s', '2024-04-18', 63.27, 'Mercado Livre', 0),
-(13, 'Supernova', '2024-05-12', 70.00, 'Mercado Livre', 1),
-(14, 'Indie Vibes', '2024-06-15', 37.50, 'Universal Music Store', 0),
-(15, 'Pirata', '2024-07-25', 69.90, 'Loja Regards', 1);
+(1, 'Super', '2023-05-15', 43.90, 'Amazon', 1), --
+(2, 'Pilantra', '2023-03-12', 45.00, 'Amazon', 0), --
+(3, 'Supernova', '2023-07-21', 50.00, 'Universal Music Store', 1), --
+(4, 'Super', '2023-08-14', 39.90, 'Universal Music Store', 1), --
+(5, 'Vou Morrer Sozinho', '2018-08-14', 42.50, 'Amazon', 0), --
+(6, 'Louquinho', '2019-07-11', 126.53, 'Loja Regards', 0), --
+(7, 'Mal', '2024-09-10', 92.08, 'Loja Regards', 0), --
+(8, 'Super', '2025-07-12', 41.90, 'Amazon', 1), --
+(9, 'Super', '2024-01-15', 39.95, 'Mercado Livre', 1), --
+(10, 'Super', '2024-11-15', 29.90, 'Universal Music Store', 1), --
+(11, 'Supernova', '2024-06-11', 62.80, 'Mercado Livre', 1), --
+(12, 'Jardins da Babilônia', '2024-04-19', 63.27, 'Mercado Livre', 0), --
+(13, 'Supernova', '2023-07-27', 70.00, 'Mercado Livre', 1), --
+(14, 'Andar Sozinho', '2019-05-10', 37.50, 'Universal Music Store', 0), --
+(15, 'Pirata', '2021-10-19', 69.90, 'Loja Regards', 1); --
 GO
