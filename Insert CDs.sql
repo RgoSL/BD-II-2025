b4db3edd588ba444d6dfae3f3faa39bd8437d811
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-03-31 16:05:02 -0300

    Mais commit no Insert do CD

--- Insert CDs.sql
+++ Insert CDs.sql
@@ -1,19 +1,19 @@
 INSERT INTO CDs
 ([Codigo], [NomeCD], [DataCompra], [ValorPago], [LocalCompra], [Album])
 VALUES
-(1, 'Thriller', '2023-05-15', 43.90, 'Amazon', 1),
-(2, 'Back In Black', '2023-06-10', 45.00, 'Amazon', 1),
-(3, 'Abbey Road', '2023-07-20', 50.00, 'Universal Music Store', 1),
-(4, 'Rumours', '2023-08-25', 39.90, 'Universal Music Store', 1),
-(5, 'The Dark Side of the Moon', '2023-09-30', 42.50, 'Amazon', 1),
-(6, 'Let It Be', '2023-10-05', 126.53, 'Loja Regards', 1),	
-(7, 'Born to Run', '2023-11-10', 92.08, 'Loja Regards', 1),
-(8, 'Hotel California', '2023-12-01', 41.90, 'Amazon', 1),
-(9, 'The Wall', '2024-01-15', 39.95, 'Mercado Livre', 1),
-(10, 'The Best of 80s', '2024-02-05', 29.90, 'Universal Music Store', 0),
-(11, 'Classical Hits', '2024-03-10', 62.80, 'Mercado Livre', 0),
+(1, 'Super', '2023-05-15', 43.90, 'Amazon', 1), 
+(2, 'Back In Black', '2023-06-10', 45.00, 'Amazon', 0),
+(3, 'Supernova', '2023-07-20', 50.00, 'Universal Music Store', 1),
+(4, 'Super', '2023-08-25', 39.90, 'Universal Music Store', 1),
+(5, 'The Dark Side of the Moon', '2023-09-30', 42.50, 'Amazon', 0),
+(6, 'Let It Be', '2023-10-05', 126.53, 'Loja Regards', 0),	
+(7, 'Born to Run', '2023-11-10', 92.08, 'Loja Regards', 0),
+(8, 'Super', '2023-12-01', 41.90, 'Amazon', 1),
+(9, 'Super', '2024-01-15', 39.95, 'Mercado Livre', 1),
+(10, 'Super', '2024-02-05', 29.90, 'Universal Music Store', 1),
+(11, 'Supernova', '2024-03-10', 62.80, 'Mercado Livre', 1),
 (12, 'Top Hits 90s', '2024-04-18', 63.27, 'Mercado Livre', 0),
-(13, 'Rock Legends', '2024-05-12', 70.00, 'Mercado Livre', 0),
+(13, 'Supernova', '2024-05-12', 70.00, 'Mercado Livre', 1),
 (14, 'Indie Vibes', '2024-06-15', 37.50, 'Universal Music Store', 0),
-(15, 'Jazz Essentials', '2024-07-25', 69.90, 'Loja Regards', 0);
+(15, 'Pirata', '2024-07-25', 69.90, 'Loja Regards', 1);
 GO
