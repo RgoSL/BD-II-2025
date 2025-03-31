011c32e3ce4ad9b825c3cc33ceb0f0a9bba4cf13
Rodrigo 🤙 <125403873+RgoSL@users.noreply.github.com>
2025-03-31 15:29:35 -0300

    Add metade dos Inserts da Tabela CD

--- Insert CDs.sql
+++ Insert CDs.sql
@@ -1,11 +1,19 @@
-USE Locadora
-
 INSERT INTO CDs
-([Codigo],[NomeCd],[DataCompra],[ValorPago],[LocalCompra],[Album])
+([Codigo], [NomeCD], [DataCompra], [ValorPago], [LocalCompra], [Album])
 VALUES
-(1,),
-(2,),
-(3,),
-(4,),
-(5,);
+(1, 'Thriller', '2023-05-15', 39.90, 'Amazon', 1),
+(2, 'Back In Black', '2023-06-10', 45.00, 'Amazon', 1),
+(3, 'Abbey Road', '2023-07-20', 50.00, 'Universal Music Store', 1),
+(4, 'Rumours', '2023-08-25', 39.90, 'Universal Music Store', 1),
+(5, 'The Dark Side of the Moon', '2023-09-30', 42.50, 'Amazon', 1),
+(6, 'Let It Be', '2023-10-05', 34.90, 'Loja Regards', 1),
+(7, 'Born to Run', '2023-11-10', 48.00, 'Loja Regards', 1),
+(8, 'Hotel California', '2023-12-01', 38.90, 'Amazon', 1),
+(9, 'The Wall', '2024-01-15', 46.00, 'Mercado Livre', 1),
+(10, 'The Best of 80s', '2024-02-05', 29.90, 'Universal Music Store', 0),
+(11, 'Classical Hits', '2024-03-10', 25.00, 'Mercado Livre', 0),
+(12, 'Top Hits 90s', '2024-04-18', 33.90, 'Mercado Livre', 0),
+(13, 'Rock Legends', '2024-05-12', 40.00, 'Mercado Livre', 0),
+(14, 'Indie Vibes', '2024-06-15', 37.50, 'Universal Music Store', 0),
+(15, 'Jazz Essentials', '2024-07-25', 29.90, 'Loja Regards', 0);
 GO
